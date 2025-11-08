7befa8940927ace86509ad987ca9797b124c5cc3
Екатерина Бочарова <kati.bocharova.06@mail.ru>
2025-11-08 21:30:35 +0500

    Task1

---
+++ Tyuiu.BocharovaES.Sprint4.Task1.V20.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.BocharovaES.Sprint4.Task1.V20.Lib
+{
+    public class DataService : ISprint4Task1V20
+    {
+        public int Calculate(int[] array)
+        {
+            int res = 0;
+            for(int i =0; i <= array.Length-1; i++)
+            {
+                if (array[i]%2 != 0)
+                {
+                    res += array[i];
+                }
+            }
+            return res;
+        }
+    }
+}

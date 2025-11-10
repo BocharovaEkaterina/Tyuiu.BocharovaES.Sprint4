d2dbd233bcb1058da45df1614fa655937646e7e6
Екатерина Бочарова <kati.bocharova.06@mail.ru>
2025-11-10 18:10:52 +0500

    Task5

---
+++ Tyuiu.BocharovaES.Sprint4.Task5.V1.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.BocharovaES.Sprint4.Task5.V1.Lib;
+namespace Tyuiu.BocharovaES.Sprint4.Task5.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[,] mas2 = { { 2, 5, -5 }, 
+                            { -5, 5, 0 },
+                            { -7, 5, 4 }};
+            int[,] res = ds.Calculate(mas2);
+            int[,] wait = { { 1, 1, -5 },
+                            { -5, 1, 0 },
+                            { -7, 1, 1 }};
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BocharovaES.Sprint4.Task5.V1/Program.cs
@@ -0,0 +1,76 @@
+﻿using Tyuiu.BocharovaES.Sprint4.Task5.V1.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Random rnd = new Random();
+
+        Console.Title = "Спринт #4 | Выполнила: Бочарова Е. С. | ИИПб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Двумерные массивы(генератор случайных чисел)                      *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #1                                                              *");
+        Console.WriteLine("* Выполнила: Бочарова Екатерина Сергеевна | ИИПб-25-1                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ :                                                               *");
+        Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+        Console.WriteLine("* случайными значениями в диапазоне от -9 до 8. Заменить положительные    *");
+        Console.WriteLine("* элементы на 1.                                                      *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ :                                                       *");
+        Console.WriteLine("***************************************************************************");
+
+        int rows, columns;
+
+        Console.Write("Введите количество строк в массиве: ");
+        rows = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите количество столбцов в массиве: ");
+        columns = Convert.ToInt32(Console.ReadLine());
+
+        int[,] mtrx = new int[rows, columns];
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                mtrx[i, j] = rnd.Next(-9, 8);
+            }
+        }
+
+        Console.WriteLine("\nМассив: ");
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"{mtrx[i, j]} \t");
+            }
+            Console.WriteLine();
+        }
+
+
+        Console.WriteLine();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        int[,] res = ds.Calculate(mtrx);
+
+        Console.WriteLine("Полученная матрица: ");
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"{res[i, j]} \t");
+            }
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+    }
+}

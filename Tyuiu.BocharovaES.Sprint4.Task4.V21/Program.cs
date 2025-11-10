43fcb4b1b1d8f8f886d96259c72f4890946ff329
Екатерина Бочарова <kati.bocharova.06@mail.ru>
2025-11-10 16:09:32 +0500

    Task4

---
+++ Tyuiu.BocharovaES.Sprint4.Task4.V21/Program.cs
@@ -0,0 +1,67 @@
+﻿using Tyuiu.BocharovaES.Sprint4.Task4.V21.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #4 | Выполнила: Бочарова Е. С. | ИИПб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Двумерные массивы(ввод с клавиатуры)                             *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #21                                                             *");
+        Console.WriteLine("* Выполнила: Бочарова Екатерина Сергеевна | ИИПб-25-1                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ :                                                               *");
+        Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+        Console.WriteLine("* значениями с клавиатуры в диапазоне от 3 до 8. Найдите сумму четных     *");
+        Console.WriteLine("* элементов массива.                                                      *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ :                                                       *");
+        Console.WriteLine("***************************************************************************");
+
+        int rows, columns;
+
+        Console.Write("Введите количество строк в массиве: ");
+        rows = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите количество столбцов в массиве: ");
+        columns = Convert.ToInt32(Console.ReadLine());
+
+        int[,] mtrx = new int[rows, columns];
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"Введите {i},{j} элемент массива: ");
+                mtrx[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        Console.WriteLine("\nМассив: ");
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"{mtrx[i, j]} \t");
+            }
+            Console.WriteLine();
+        }
+
+
+        Console.WriteLine();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        int res = ds.Calculate(mtrx);
+
+        Console.WriteLine("Cумма четных элементов массива: " + res );
+        Console.ReadKey();
+    }
+}
